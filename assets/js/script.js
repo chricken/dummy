3028a80e372962c95488231aecdf851aa4307738
chricken <post@bitschmiede.com>
2026-04-24 12:06:29 +0200

    Äußere Planeten eingefügt

--- assets/js/script.js
+++ assets/js/script.js
@@ -1,7 +1,11 @@
 'use strict';
 
 const init = () => {
-
+    'Jupiter, Saturn, Uranus, Neptun'.split(', ').forEach(planet => {
+        const el = document.createElement('p');
+        el.innerText = planet;
+        document.body.append(el);
+    })
 }
 
 init();
